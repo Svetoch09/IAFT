7da12ceaae9d0f86e99967a9f15bdc12fde0256b
Svetlana Paskevich <strays-binges.4d@icloud.com>
2025-09-24 15:42:10 +0200

    New gitignore + pom

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,36 +1,43 @@
 package tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import parent.BaseTest;
-
-import java.time.Duration;
-
 import static org.testng.Assert.*;
 
 public class LoginTest extends BaseTest {
 
-    @Test
-    public void testSuccessfulLogin() {
+    @Test(description = "Successful login", priority = 1)
+    public void checkSuccessfulLogin() {
         loginPage.open();
         loginPage.login("standard_user", "secret_sauce");
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement productsHeader = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Products']")));
-        assertTrue(productsHeader.isDisplayed(), "Вход не выполнен");
+        assertTrue(productsPage.isTitlePresent());
+        assertEquals(productsPage.getTitle(), "Products", "Product title is not present");
+        productsPage.addToCart("Sauce Labs Backpack");
+        //loginPage.open("inventory.html");
     }
 
-    @Test
-    public void testUserError() {
+    @Test(description = "Wrong username error",  priority = 2)
+    public void checkUserNameError() {
         loginPage.open();
         loginPage.login("useruser", "secret_sauce");
+        assertEquals(loginPage.checkErrorMsg(), "Epic sadface: Username and password do not match any user in this service",
+                "Error is not correct");
+    }
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement errorElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h3[data-test='error']")));
-        String expectedError = "Epic sadface: Username and password do not match any user in this service";
-        assertEquals(errorElement.getText(), expectedError, "Текст ошибки неверный.");
+    @Test(description = "Locked out user error",  priority = 4)
+    public void checkUserLockedOutError() {
+        loginPage.open();
+        loginPage.login("locked_out_user", "secret_sauce");
+        assertEquals(loginPage.checkErrorMsg(), "Epic sadface: Sorry, this user has been locked out.",
+                "Error is not correct");
+    }
+
+    @Test(description = "No password error",   priority = 3)
+    public void checkNoPasswordError() {
+        loginPage.open();
+        loginPage.login("standard_user", "");
+        assertEquals(loginPage.checkErrorMsg(), "Epic sadface: Password is required",
+                "Error is not correct");
     }
 }
