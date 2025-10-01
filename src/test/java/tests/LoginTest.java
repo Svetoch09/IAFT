a41309ec88a88c0f247c4ec1176ddff172c28dfc
Svetlana Paskevich <strays-binges.4d@icloud.com>
2025-10-01 17:29:49 +0200

    config.properties, PropertyReader, User

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,32 +3,44 @@ package tests;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import parent.BaseTest;
+import utils.PropertyReader;
+import user.User;
 
 import static org.testng.Assert.*;
+import static user.UserFactory.*;
 
 public class LoginTest extends BaseTest {
 
     @Test(description = "Successful login", priority = 1)
     public void checkSuccessfulLogin() {
+        System.out.println("Checking Successful Login in thread: " + Thread.currentThread().getName());
         loginPage.open();
-        loginPage.login("standard_user", "secret_sauce");
-
+        loginPage.login(withAdminPermission());
         assertTrue(productsPage.isTitlePresent());
     }
 
     @DataProvider(name = "inc")
     public Object[][] loginData() {
+        String invalidMsg = PropertyReader.getProperty("invalid.error.msg");
+        String lockedMsg = PropertyReader.getProperty("locked.error.msg");
+        String emptyPassMsg = PropertyReader.getProperty("empty_password.error.msg");
+
+        User invalidUser = withInvalidUserPermission();
+        User lockedUser = withLockedUserPermission();
+        User emptyPassUser = withUserEmptyPasswordPermission();
+
         return new Object[][]{
-                {"useruser", "secret_sauce", "Epic sadface: Username and password do not match any user in this service"},
-                {"locked_out_user", "secret_sauce", "Epic sadface: Sorry, this user has been locked out."},
-                {"standard_user", "", "Epic sadface: Password is required"}
+                {invalidUser, invalidMsg},
+                {lockedUser, lockedMsg},
+                {emptyPassUser, emptyPassMsg}
         };
     }
 
     @Test(dataProvider = "inc", description = "Wrong login data", priority = 2)
-    public void checkIncorrectData(String username, String password, String errorMessage) {
+    public void checkIncorrectData(User user, String errorMessage) {
+        System.out.println("Checking unsuccessful Login in thread: " + Thread.currentThread().getName());
         loginPage.open();
-        loginPage.login(username, password);
+        loginPage.login(user);
         assertEquals(loginPage.checkErrorMsg(), errorMessage);
     }
 }
